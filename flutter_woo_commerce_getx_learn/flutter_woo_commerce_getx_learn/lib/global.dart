6c37838f9a25c547c02fb4220e777f01b5665b45
changyuan <changyuan>
2024-10-14 15:55:59 +0800

    config service

---
+++ flutter_woo_commerce_getx_learn/flutter_woo_commerce_getx_learn/lib/global.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'common/index.dart';
+
+class Global {
+  static Future<void> init() async {
+    // Flutter 与原生端 的接口初始化
+    WidgetsFlutterBinding.ensureInitialized();
+
+
+    // 将Get  压到内存中初始化
+    Get.put<ConfigService>(ConfigService());
+  }
+}
+
