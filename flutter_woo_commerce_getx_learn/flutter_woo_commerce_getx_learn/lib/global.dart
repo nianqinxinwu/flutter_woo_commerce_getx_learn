1543add496a6d537153c48be3b28c2da0dbaab29
changyuan <changyuan>
2024-10-15 09:20:38 +0800

    网络请求dio封装初始化

--- flutter_woo_commerce_getx_learn/flutter_woo_commerce_getx_learn/lib/global.dart
+++ flutter_woo_commerce_getx_learn/flutter_woo_commerce_getx_learn/lib/global.dart
@@ -7,12 +7,14 @@ class Global {
     // Flutter 与原生端 的接口初始化
     WidgetsFlutterBinding.ensureInitialized();
 
+    // 本地存储初始化(工具类)
+    await Storage().init();
 
     // 将Get  压到内存中初始化
     Get.put<ConfigService>(ConfigService());
 
-    // 本地存储初始化(工具类)
-    await Storage().init();
+    // 网络请求初始化
+    Get.put<WPHttpService>(WPHttpService());
   }
 }
 
