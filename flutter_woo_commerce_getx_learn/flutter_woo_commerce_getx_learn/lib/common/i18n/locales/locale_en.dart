e8ca08350bd3764036057f9e3d7efa05276a6854
changyuan <changyuan>
2024-10-21 10:01:22 +0800

    商品首页布局 sliver 编写

--- flutter_woo_commerce_getx_learn/flutter_woo_commerce_getx_learn/lib/common/i18n/locales/locale_en.dart
+++ flutter_woo_commerce_getx_learn/flutter_woo_commerce_getx_learn/lib/common/i18n/locales/locale_en.dart
@@ -74,4 +74,11 @@ Map<String, String> localeEn = {
   LocaleKeys.tabBarMessage: 'Message',
   LocaleKeys.tabBarProfile: 'Profile',
 
+  // 商品 - 首页
+  LocaleKeys.gHomeSearch: 'Search Product',
+  LocaleKeys.gHomeFlashSell: 'Flash Sell',
+  LocaleKeys.gHomeNewProduct: 'New Product',
+  LocaleKeys.gHomeMore: 'ALL',
+
+
 };
