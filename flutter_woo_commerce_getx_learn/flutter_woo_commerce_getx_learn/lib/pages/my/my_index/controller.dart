3fa2674a0e2fd4fc52fa7cda91fc446e682fdaaf
changyuan <changyuan>
2024-11-14 12:24:32 +0800

    我的页面 - 功能列表、注销

--- flutter_woo_commerce_getx_learn/flutter_woo_commerce_getx_learn/lib/pages/my/my_index/controller.dart
+++ flutter_woo_commerce_getx_learn/flutter_woo_commerce_getx_learn/lib/pages/my/my_index/controller.dart
@@ -1,7 +1,14 @@
 import 'package:get/get.dart';
-
+import 'package:flutter_woo_commerce_getx_learn/common/index.dart';
+import 'package:flutter_woo_commerce_getx_learn/pages/index.dart';
 class MyIndexController extends GetxController {
   MyIndexController();
+
+    // 注销
+  void onLogout() {
+    UserService.to.logout();
+    Get.find<MainController>().onJumpToPage(0);
+  }
 
   _initData() {
     update(["my_index"]);
